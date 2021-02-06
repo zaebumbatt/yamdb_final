a90a4d5910db5467603a12c1bffd501a0234ac65
Nikita <zaebumba@gmail.com>
2021-02-06 08:21:51 +0700

    Update100

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Дополнения для проекта
 
